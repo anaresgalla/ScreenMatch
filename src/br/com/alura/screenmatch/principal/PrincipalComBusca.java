d1b9d20f00bd4ffc78e0b57a8e859d1b04bff77e
anaresgalla <anaresgalla@gmail.com>
2025-11-13 11:31:19 -0300

    Refina a busca

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,12 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o título para busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=a0b5e147";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=a0b5e147"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
